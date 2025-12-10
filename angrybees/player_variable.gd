6ba47954a0b16e22e991800a5b008b9c983fac41
Renae Hunt <jerseeh2003@gmail.com>
2025-12-09 17:57:35 -0700

    Revert "Fixing Issue with Merge"

--- angrybees/player_variable.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var bear_speed
-var pollen_inv := 0
--- angrybees/pollen_mini_game/pollen_script/pollen.gd
+++
@@ -1,7 +0,0 @@
-extends RigidBody2D
-
-func _on_pickup_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		print("Pick up!")
-		PlayerVariable.pollen_inv += 1
-		queue_free() 
